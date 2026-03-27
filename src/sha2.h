cd9c84503970b8d944fce85ca6634fee2c08f83e
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2026-03-26 23:57:59 -0400

    initial code in header file for sha512 support

--- src/sha2.h
+++ src/sha2.h
@@ -36,6 +36,9 @@ SOFTWARE.
 #define ROTR_U32(data, n_bits) \
         ((data << ((sizeof(uint32_t) * 8) - n_bits)) | (data >> n_bits))
 
+#define ROTR_U64(data, n_bits) \
+        ((data << ((sizeof(uint64_t) * 8) - n_bits)) | (data >> n_bits))
+
 
 #define SWAP_ENDIANESS_U32(data) \
         (((data & 0x000000ff) << 24) |  \
@@ -57,6 +60,27 @@ static uint32_t SHA256_K[] = {
 #define N_SHA256_Ks \
         (sizeof(SHA256_K) / sizeof(uint32_t))
 
+
+static uint64_t SHA512_K[] = {
+    0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc, 0x3956c25bf348b538, 
+    0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118, 0xd807aa98a3030242, 0x12835b0145706fbe, 
+    0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2, 0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 
+    0xc19bf174cf692694, 0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65, 
+    0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5, 0x983e5152ee66dfab, 
+    0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4, 0xc6e00bf33da88fc2, 0xd5a79147930aa725, 
+    0x06ca6351e003826f, 0x142929670a0e6e70, 0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 
+    0x53380d139d95b3df, 0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b, 
+    0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30, 0xd192e819d6ef5218, 
+    0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8, 0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 
+    0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8, 0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 
+    0x682e6ff3d6b2b8a3, 0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec, 
+    0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b, 0xca273eceea26619c, 
+    0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178, 0x06f067aa72176fba, 0x0a637dc5a2c898a6, 
+    0x113f9804bef90dae, 0x1b710b35131c471b, 0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 
+    0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
+};
+#define N_SHA512_Ks \
+        (sizeof(SHA512_K) / sizeof(uint64_t))
 
 #define CALCULATE_SHA256_k(input_bitlen) \
         (SHA256_CHUNK_BITSIZE - ((input_bitlen + 1 + (sizeof(uint64_t) * 8)) % SHA256_CHUNK_BITSIZE))
@@ -85,12 +109,10 @@ static uint32_t SHA224_INIT_HASH[] = {
 
 typedef struct sha224 sha224;
 struct sha224{
-    struct {
-        union{
-            uint32_t w0_63[N_SHA256_Ks];
-            uint32_t w0_15[N_SHA256_Ks / 4];
-            uint8_t buffer_u8[SHA224_CHUNK_BYTESIZE];
-        };
+    union{
+        uint32_t w0_63[N_SHA256_Ks];
+        uint32_t w0_15[N_SHA256_Ks / 4];
+        uint8_t buffer_u8[SHA224_CHUNK_BYTESIZE];
     };
     uint32_t hash[SHA224_HASH_U32WORDS];
     uint64_t data_bytelen;
@@ -129,12 +151,10 @@ static uint32_t SHA256_INIT_HASH[] = {
 
 typedef struct sha256 sha256;
 struct sha256{
-    struct {
-        union{
-            uint32_t w0_63[N_SHA256_Ks];
-            uint32_t w0_15[N_SHA256_Ks / 4];
-            uint8_t buffer_u8[SHA256_CHUNK_BYTESIZE];
-        };
+    union{
+        uint32_t w0_63[N_SHA256_Ks];
+        uint32_t w0_15[N_SHA256_Ks / 4];
+        uint8_t buffer_u8[SHA256_CHUNK_BYTESIZE];
     };
     uint32_t hash[SHA256_HASH_U32WORDS];
     uint64_t data_bytelen;
@@ -149,6 +169,42 @@ int sha256_chain(sha256*, uint8_t*, uint64_t);
 int sha256_end(sha256*);
 int sha256_delete(sha256*);
 int sha256_stringify_hash(sha256*, char*);
+
+
+/*===================================================================*/
+/*                              SHA-512                              */
+/*===================================================================*/
+static uint64_t SHA512_INIT_HASH[] = {
+    0x6a09e667f3bcc908,
+    0xbb67ae8584caa73b,
+    0x3c6ef372fe94f82b,
+    0xa54ff53a5f1d36f1, 
+    0x510e527fade682d1,
+    0x9b05688c2b3e6c1f,
+    0x1f83d9abfb41bd6b,
+    0x5be0cd19137e2179
+};
+#define SHA512_HASH_BYTESIZE (sizeof(SHA512_INIT_HASH))
+#define SHA512_HASH_BITSIZE (SHA512_HASH_BYTESIZE * 8)
+#define SHA512_HASH_U64WORDS (SHA512_HASH_BYTESIZE / sizeof(uint64_t))
+#define SHA512_CHUNK_BITSIZE (SHA512_HASH_BITSIZE * 2)
+#define SHA512_CHUNK_BYTESIZE (SHA512_CHUNK_BITSIZE / 8)
+
+
+typedef struct sha512 sha512;
+struct sha512{
+    union{
+        uint64_t w0_80[N_SHA512_Ks];
+        uint64_t w0_15[N_SHA512_Ks / 4];
+        uint8_t buffer_u8[SHA512_CHUNK_BYTESIZE];
+    };
+    uint64_t hash[SHA512_HASH_U64WORDS];
+    __uint128_t data_bytelen;
+    bool big_endian;
+    bool done;
+    uint8_t buffer_idx;
+};
+
 
 
 #endif
