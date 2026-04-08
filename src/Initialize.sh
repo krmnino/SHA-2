059bfefeb61cbf38413e82b8595d76eefd86e5f0
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2026-04-08 01:31:26 -0400

    include initializer script in src

---
+++ src/Initialize.sh
@@ -0,0 +1,12 @@
+# If bin directory doesn't exist, then create it.
+if [ ! -d ../bin ]; then
+    mkdir ../bin
+    echo Created ../bin directory 
+fi 
+
+# If res directory doesn't exist, then create it.
+if [ ! -d ../res ]; then
+    mkdir ../res
+    echo Created ../res directory 
+fi 
+
