9b6a2fcac1967604110354fb1238ed79cfb8f068
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2026-04-28 01:14:32 -0400

    initial code for testcase generator

--- testing/Context.h
+++ testing/Context.h
@@ -4,11 +4,35 @@
 #include <stdint.h>
 #include <string.h>
 #include "../res/ArgParsing_C.h"
+#include "../res/Randomizer_C.h"
 #include "../src/sha2.h"
 
 
+//#define SHA224_ARG     0x0000000000000001
+//#define SHA256_ARG     0x0000000000000002
+//#define SHA384_ARG     0x0000000000000004
+//#define SHA512_ARG     0x0000000000000008
+//#define SHA512_224_ARG 0x0000000000000010
+//#define SHA512_256_ARG 0x0000000000000020
+
+typedef enum SHA_Algs SHA_Algs;
+enum SHA_Algs {
+    SHA224_ALG     = 0x0000000000000001,
+    SHA256_ALG     = 0x0000000000000002,
+    SHA384_ALG     = 0x0000000000000004,
+    SHA512_ALG     = 0x0000000000000008,
+    SHA512_224_ALG = 0x0000000000000010,
+    SHA512_256_ALG = 0x0000000000000020,
+};
+#define NUM_ALGORITHMS 6
+
 static struct Context{
     ArgParsing_C* ap;
+    Randomizer_C* rnd;
+    uint64_t algorithms;
+    uint64_t n_tests;
+    uint32_t seed;
+    bool trace;
 } Context;
 
 
--- testing/Main.c
+++ testing/Main.c
@@ -1,4 +1,30 @@
+/*
+MIT License
+
+Copyright (c) 2026 Kurt Manrique-Nino
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
 #include "Context.h"
+
 
 int process_args(int argc, char* argv[]){
     int ret;
@@ -7,31 +33,51 @@ int process_args(int argc, char* argv[]){
 
     // Define table of arguments
     APTableEntry_C arg_table[] = {
-        { .abbr_form="a", .full_form="algorithms", .initialized=false, .data_type=UNSIGNED_INT, .required=false , .default_value=true, .data.number_u64=0x3f  },
-        { .abbr_form="t", .full_form="trace", .initialized=false, .data_type=FLAG, .required=false , .default_value=true, .data.flag=true  },
+        { .abbr_form="a", .full_form="algorithms", .initialized=false, .data_type=UNSIGNED_INT, .required=false, .default_value=true , .data.number_u64=0x000000000000003f  },
+        { .abbr_form="s", .full_form="seed"      , .initialized=false, .data_type=UNSIGNED_INT, .required=true , .default_value=false, .data={0}  },
+        { .abbr_form="n", .full_form="n_tests"   , .initialized=false, .data_type=UNSIGNED_INT, .required=true , .default_value=false, .data={0}  },
+        { .abbr_form="t", .full_form="trace"     , .initialized=false, .data_type=FLAG        , .required=false, .default_value=false, .data.flag=true  },
     };
 
-    // Establish table of arguments
+    // Establish table of arguments, argc, and argv
     ret = ArgParsing_C_set_arg_table(Context.ap, arg_table, sizeof(arg_table) / sizeof(APTableEntry_C));
     if(ret != 0){
         printf("ERROR: ArgParsing_C_set_arg_table() return code -> %d\n", ret);
         return -1;
     }
-    
     ArgParsing_C_set_input_args(Context.ap, argc, argv);
     
+    // Parse the program arguments
     ret = ArgParsing_C_parse(Context.ap);
     if(ret != 0){
         printf("ERROR: ArgParsing_C_parse() return code -> %d\n", ret);
         return -1;
     }
 
+    // Obtain argument values into Context structure
+    Context.algorithms = ArgParsing_C_get_value_UNSIGNED_INT(Context.ap, "algorithms", false);
+    if((Context.algorithms & (~(uint64_t)0x3f)) != 0){
+        printf("ERROR: -a/--algorithm argument value should not exceed 0x3f.\n");
+        printf("The following values represent SHA-2 algorithms and can be bit-wise OR'd:\n");
+        printf(" SHA-224     = 0x01\n");
+        printf(" SHA-256     = 0x02\n");
+        printf(" SHA-384     = 0x04\n");
+        printf(" SHA-512     = 0x08\n");
+        printf(" SHA-512/224 = 0x10\n");
+        printf(" SHA-512/256 = 0x20\n");
+        return -1;
+    }
+    Context.seed = (uint32_t)ArgParsing_C_get_value_UNSIGNED_INT(Context.ap, "seed", false);
+    Context.n_tests = ArgParsing_C_get_value_UNSIGNED_INT(Context.ap, "n_tests", false);
+    Context.trace = ArgParsing_C_get_value_FLAG(Context.ap, "trace", false);
     return 0;
 }
 
 
 int main(int argc, char* argv[]){
     int ret;
+    uint64_t shifter;
+    SHA_Algs picked_alg;
 
     // Clear the Context instance
     memset((void*)&Context, 0, sizeof(Context));
@@ -40,6 +86,37 @@ int main(int argc, char* argv[]){
     ret = process_args(argc, argv);
     if(ret != 0){
         return -1;
+    }
+
+    // Initialize Randomizer
+    Context.rnd = Randomizer_C_init(Context.seed);
+
+    for(size_t i = 0; i < Context.n_tests; i++){
+        shifter = Randomizer_C_gen_integral_range(Context.rnd, 0, (NUM_ALGORITHMS - 1));
+        picked_alg = (SHA_Algs)(1 << shifter);
+        switch(picked_alg){
+        case SHA224_ALG:
+            printf(">> %ld - picked SHA224\n", i);
+            break;
+        case SHA256_ALG:
+            printf(">> %ld - picked SHA256\n", i);
+            break;
+        case SHA384_ALG:
+            printf(">> %ld - picked SHA384\n", i);
+            break;
+        case SHA512_ALG:
+            printf(">> %ld - picked SHA512\n", i);
+            break;
+        case SHA512_224_ALG:
+            printf(">> %ld - picked SHA512/224\n", i);
+            break;
+        case SHA512_256_ALG:
+            printf(">> %ld - picked SHA512/256\n", i);
+            break;
+        default:
+            break;
+        }
+        Randomizer_C_root_seed_next(Context.rnd);
     }
 
     return 0;
