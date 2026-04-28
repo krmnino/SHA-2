829348b8802acd4d039ae15b992b9a3b3e219bb1
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2026-04-28 01:16:10 -0400

    updated testing makefile and intializer script to include Randomizer library

--- testing/Initialize.sh
+++ testing/Initialize.sh
@@ -22,3 +22,16 @@ make -C ../res/ArgParsing-$ArgParsingVer/src
 cp ../res/ArgParsing-$ArgParsingVer/src/ArgParsing_C.h ../res
 cp ../res/ArgParsing-$ArgParsingVer/res/libArgParsing.so ../res
 rm -r ../res/ArgParsing-$ArgParsingVer
+
+# Unpack and build Randomizer
+RandomizerVer=1.4.1
+wget -O ../res/Randomizer_v$RandomizerVer.tar.gz https://github.com/krmnino/Randomizer/archive/refs/tags/v$RandomizerVer.tar.gz
+tar -xvzf ../res/Randomizer_v$RandomizerVer.tar.gz --directory ../res
+rm -r ../res/Randomizer_v$RandomizerVer.tar.gz
+cd ../res/Randomizer-$RandomizerVer/src
+sh Initialize.sh
+cd -
+make -C ../res/Randomizer-$RandomizerVer/src
+cp ../res/Randomizer-$RandomizerVer/src/Randomizer_C.h ../res
+cp ../res/Randomizer-$RandomizerVer/res/libRandomizer.so ../res
+rm -r ../res/Randomizer-$RandomizerVer
