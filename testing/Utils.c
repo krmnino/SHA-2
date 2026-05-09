7e8597f35a02ae3f7877bc92618fcb423ef46094
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2026-05-08 22:12:57 -0400

    utility functions

---
+++ testing/Utils.c
@@ -0,0 +1,112 @@
+#include "Utils.h"
+
+
+int hex_str_2_bin_str(char* input_buffer, uint8_t* output_buffer, size_t input_charlen){
+    uint8_t byte;
+
+    // Validate input
+    if(input_buffer == NULL){
+        printf("ERROR: Pointer to input buffer is NULL.");
+        return -1;
+    }
+    if(output_buffer == NULL){
+        printf("ERROR: Pointer to output buffer is NULL.");
+        return -1;
+    }
+    if(input_charlen == 0){
+        printf("ERROR: Input buffer character length value is 0.");
+        return -1;    
+    }
+
+    for(size_t i = 0; i < input_charlen; i++){
+        switch (input_buffer[i]) {
+        case 'A':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+            byte = ((input_buffer[i] & 0x0F) | 0x08) + 1;
+            break;        
+        default:
+            byte = input_buffer[i] & 0x0F;
+            break;
+        }
+        if((i % 2) == 0){
+            output_buffer[(i / 2)] = byte << 4;
+        }
+        else{
+            output_buffer[(i / 2)] = output_buffer[(i / 2)] | byte;
+        }
+    }
+    return 0;
+}
+
+
+int bin_str_2_hex_str(uint8_t* input_buffer, char* output_buffer, size_t input_bytelen){
+    size_t output_idx;
+    char loc_buff[3];
+
+    // Validate input
+    if(input_buffer == NULL){
+        printf("ERROR: Pointer to input buffer is NULL.");
+        return -1;
+    }
+    if(output_buffer == NULL){
+        printf("ERROR: Pointer to output buffer is NULL.");
+        return -1;
+    }
+    if(input_bytelen == 0){
+        printf("ERROR: Input buffer byte length value is 0.");
+        return -1;    
+    }
+
+    output_idx = 0;
+    for(size_t i = 0; i < input_bytelen; i++){
+        sprintf(((char*)&output_buffer[output_idx]), "%02x", input_buffer[i]);
+        output_idx += 2;
+    }
+    return 0;
+}
+
+
+int hex_print(uint8_t* buff, size_t data_len, size_t addr_offset){
+    char hex_str[3];
+    size_t bytes_to_print;
+
+    // Validate input
+    if(buff == NULL){
+        printf("ERROR: SourceBucket pointer is NULL.\n");
+        return -1;
+    }
+
+    for(size_t i = 0; i < data_len; i += 4){
+        bytes_to_print = data_len - i; 
+        if(bytes_to_print > MAX_BYTES_IN_WORD){
+            bytes_to_print = MAX_BYTES_IN_WORD;
+        }
+        if(i % MAX_BYTES_IN_LINE == 0){
+            if(i != 0){
+                printf("\n%016lx: ", i + addr_offset);
+            }
+            else{
+                printf("%016lx: ", i + addr_offset);
+            }
+        }
+        for(size_t j = 0; j < bytes_to_print; j++){
+            memset(hex_str, 0, sizeof(hex_str));
+            bin_str_2_hex_str((uint8_t*)&buff[i + j], hex_str, 1);
+            printf("%s", hex_str);
+        }
+        printf(" ");
+    }
+    printf("\n");
+    
+    return 0;
+}
---
+++ testing/Utils.h
@@ -0,0 +1,20 @@
+#ifndef UTILS
+#define UTILS
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAX_BYTES_IN_LINE 32
+#define MAX_BYTES_IN_WORD 4
+
+
+int hex_str_2_bin_str(char*, uint8_t*, size_t);
+int bin_str_2_hex_str(uint8_t*, char*, size_t);
+int hex_print(uint8_t*, size_t, size_t);
+
+
+#endif
