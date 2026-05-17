e4b7ef9638f6b2eaa9f3ad71181c23729c5b6352
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2026-05-16 22:03:23 -0400

    create array of message subsections sizes for multiple calls to chaining function

--- testing/sha224_tc.c
+++ testing/sha224_tc.c
@@ -24,13 +24,41 @@ SOFTWARE.
 
 
 #include "SHA2Testing.h"
+#include "SHA224_TV.h"
 
 
 int sha224_tc(){
+    uint8_t* curr_bin_msg;
+    size_t remaining;
+    uint32_t picked_len;
     int ret;
 
-    // Perform the hashing operation
-    sha224_chain(ctxt.tc->s224, ctxt.tc->bin_msg, ctxt.tc->msg_bytelen);
+    // Only for messages of length non-zero
+    if(ctxt.tc->msg_bytelen != 0){
+        // Allocate and populate array of message sub-sections sizes
+        ctxt.tc->sub_bin_msg_sizes = (uint32_t*)calloc(ctxt.tc->msg_bytelen, sizeof(uint32_t));
+        remaining = ctxt.tc->msg_bytelen;
+        ctxt.tc->sub_bin_msg_idx = 0;
+        while(remaining != 0){
+            picked_len = Randomizer_C_gen_integral_range(ctxt.rnd, 1, remaining);
+            ctxt.tc->sub_bin_msg_sizes[ctxt.tc->sub_bin_msg_idx] = picked_len;
+            ctxt.tc->sub_bin_msg_idx++;
+            remaining -= picked_len;
+        }
+        Randomizer_C_shuffle(ctxt.rnd, (void*)ctxt.tc->sub_bin_msg_sizes, ctxt.tc->sub_bin_msg_idx, sizeof(uint32_t));
+
+        // Provide sub-sections of message to hashing function
+        curr_bin_msg = ctxt.tc->bin_msg;
+        for(size_t i = 0; i < ctxt.tc->sub_bin_msg_idx; i++){
+            sha224_chain(ctxt.tc->s224, curr_bin_msg, ctxt.tc->sub_bin_msg_sizes[i]);
+            curr_bin_msg += ctxt.tc->sub_bin_msg_sizes[i];
+        }
+    }
+    else{
+        // Perform the hashing operation
+        sha224_chain(ctxt.tc->s224, ctxt.tc->bin_msg, ctxt.tc->msg_bytelen);
+    }
+
     sha224_end(ctxt.tc->s224);
     sha224_get_hash(ctxt.tc->s224, ctxt.tc->bin_res_hash);
     sha224_get_stringified_hash(ctxt.tc->s224, ctxt.tc->str_res_hash);
